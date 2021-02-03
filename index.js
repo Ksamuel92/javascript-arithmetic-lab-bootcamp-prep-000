30a11bc7ec433b682239c54ab48b8449c02efed2
Kyle Samuel <ksamuel92@gmail.com>
2021-02-03 22:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function add (x,y) {
-  return 
+function add (a,b) {
+  return a + b;
+}
+function subtract (a,b) {
+  return a - b;
+}
+function multiply (a,b) {
+  return a*b;
+}
+function divide (a,b) {
+  return a / b;
+}
+
+function increment (n) {
+  return n+= 1;
+}
+function decrement (n) {
+  return n-= 1;
+}
+
+function makeInt (n) {
+  return makeInt
 }
